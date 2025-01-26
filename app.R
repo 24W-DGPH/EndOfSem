080ec11a3b4cf896b6eaad5285a1d42c4cdbb37d
DAAdth <dorcas.anane-amponsah@stud.th-deg.de>
2025-01-26 11:28:34 +0100

    updates
    updates to app

--- app.R
+++ app.R
@@ -1,10 +1,17 @@
 # an example of app.R
-
+library(tidyverse)
 library(shiny)
 
-source("WHO_data.R")
+source("data_exploration.R")
 
 ui <- fluidPage(
+  tags$style(HTML("
+    #text {
+      font-size: 24px;
+      font-weight: bold;
+      color: #333;
+    }
+  ")),
   
   # Application title
   titlePanel("Global Health Trends"),
@@ -13,8 +20,15 @@ ui <- fluidPage(
   sidebarLayout(
     sidebarPanel(
       selectInput(
+        inputId = "country2_id",
+        label = "Select first country",
+        choices = unique(who_data_clean$country),
+        selected = "Germany",
+        multiple = FALSE
+      ),
+      selectInput(
         inputId = "country_id",
-        label = "Select country",
+        label = "Select second country",
         choices = unique(who_data_clean$country),
         selected = "Ghana",
         multiple = FALSE
@@ -44,9 +58,10 @@ ui <- fluidPage(
     
     # Show a plot 
     mainPanel(
-      textOutput("text"),
+      textOutput("text", container = span),
       plotOutput("my_plot"),
       tableOutput("summary_table"),
+      tableOutput("filter_data"),
       
     )
   )
@@ -58,23 +73,27 @@ server <- function(input, output) {
   
   data <- who_data
   
-  filter_data <- reactive({data %>% filter(country == input$country_id)})
+  countries <-reactive({c(input$country_id,input$country2_id)})
+  
+  filter_data <- reactive({data %>% filter(country %in% countries())})
+
 
   plot_1 <- reactive({
-    ggplot(filter_data(), aes(x = year)) +
+    ggplot(filter_data(), aes(x = year, color = country)) +
       geom_line(aes_string(y = input$indicator)) +
-      geom_vline(xintercept = 2019, linetype = "dashed", color = "red", size = 0.5) +
+      geom_vline(xintercept = 2019, linetype = "dashed", color = "black", size = 0.5) +
       theme_minimal() +
       theme(axis.text.x = element_text(angle = 60, vjust = 1, hjust=1)) +
-      scale_x_continuous(breaks = seq(1999, 2023, by = 2)) 
+      scale_x_continuous(breaks = seq(1999, 2023, by = 2)) +
+      scale_color_manual(values=c("darkblue", "#E69F00"))
       #labs(title = "Life expectancy per country over time",) 
 
   })
   
-  txt <- reactive({paste("visualisation of", input$indicator, "over time in", input$country_id)})
+  txt <- reactive({paste("visualisation of", input$indicator, "over time in", input$country2_id, "and", input$country_id)})
   
   # Correlation between key indicators
-  summ_table <- reactive({
+  d <- reactive({
     tibble(
       cor_hiv_mal = cor.test(filter_data()$prev_hiv, filter_data()$prev_undernourishment)$estimate,
       cor_heal_life = cor.test(filter_data()$health_exp, filter_data()$life_expect)$estimate,
@@ -86,14 +105,21 @@ server <- function(input, output) {
   })
   
   output$summary_table <- renderTable({
-    if (input$show_data == TRUE){
-    filter_data()
+    if (input$summ_stats == TRUE){
+    d()
     }
   })
   
   output$text <- renderText({
     txt()
   })
+  
+  output$filter_data <- renderTable({
+    if (input$show_data == TRUE){
+      filter_data()
+    }
+  })
+  
   
 }
 
